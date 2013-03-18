6711f558faf5c08357b33a7afbfbffe7ea458f07
Tran Xuan Truong <tranxuantruong@MacBook-Pro.local>
2013-03-18 11:02:42 +0700

    add inputOption() to read from user input

--- main.cpp
+++ main.cpp
@@ -9,16 +9,13 @@ using std::endl;
 using std::tolower;
 using std::string;
 
+const char errorCharacter = '!';
+
 // Function main()
 // Main executable
 int main(int argc, char *argv[])
 {
 	// VARIABLES USED FOR THIS PROGRAM
-	// inputOption: stores what the user types in. After that the program haves
-	// to check. If the string length > 1 then ask the user to reinput.
-	// Otherwise, get the first character and then put into the userOption
-	// variable below
-	string inputOption;
 	// userOption: stores the user option. The program must handle both
 	// upperand lowercase so that this variable will save only lowercase value.
 	// The program have to change it to lowercase before saving
@@ -32,12 +29,21 @@ int main(int argc, char *argv[])
 		displayPrompt();
 		
 		// Read user input
+		userOption = inputOption();
+
+		// Switch the user to the right option
+		switchOption(userOption);
 		
-		
-	} while (inputOption != 'q');
+	} while (userOption != 'q');
 	
 	// End the program
 	return 0;
+}
+
+// Function switchOption()
+// Switch user to the right option
+void switchOption(char userOption){
+	
 }
 
 // Function displayPrompt()
@@ -47,7 +53,27 @@ void displayPrompt(){
 }
 
 // Function inputOption()
-// Read from user input
-void inputOption(std::string&, char*){
+// Read from user input and then return the input value
+// If error, return constant errorCharacter
+char inputOption(){
+	
+	// inputOption: stores what the user types in. After that the program haves
+	// to check. If the string length > 1 then ask the user to reinput.
+	// Otherwise, get the first character and then put into the userOption
+	// variable below
+	string inputOption;
+
+	// Read from user input
+	cin >> inputOption;
+
+	// Check the inputOption length, if it's greater than 1, return
+	// errorCharacter
+	if (inputOption.length() > 1)
+	{
+		return errorCharacter;
+	} else {
+		// If its length is 1, return the first character
+		return inputOption.at(0);
+	}
 	
 }
--- main.h
+++ main.h
@@ -1,5 +1,8 @@
 /* header file for main.cpp file */
 /* contains function definitions */
 
+
+
 void displayPrompt();
-void inputOption(std::string&, char*);
+char inputOption();
+void switchOption(char);
