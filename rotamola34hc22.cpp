821ed5d1a06b88251a26d4fa4564d8644f8c0efe
Tran Xuan Truong <tranxuantruong@MacBook-Pro.local>
2013-03-25 16:36:01 +0700

    switch user to the right function for each opcode

--- rotamola34hc22.cpp
+++ rotamola34hc22.cpp
@@ -1,4 +1,10 @@
+#include <iostream>
 #include "rotamola34hc22.h"
+
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
 
 // Implementation of Rotomola34HC22 class
 namespace mcontroller {
@@ -34,12 +40,44 @@ namespace mcontroller {
 
 	// execute the instruction coorecsponding to the opcode
 	void Rotamola34HC22::execute(){
-		
+		this->executeFromLocation(this->getProgramCounter());
 	}
 
 	// execute from a specific location in memory
 	void Rotamola34HC22::executeFromLocation(int address){
-		
+		// get the opcode
+		unsigned char opcode = this->getMemoryValueAtLocation(address);
+
+		// switch user to the right function
+		switch(opcode){
+		case 0x0C:				// move A to memory
+
+			break;
+		case 0x37:				// load A with value
+
+			break;
+		case 0x38:				// load B with value
+
+			break;
+		case 0x53:				// increment A
+
+			break;
+		case 0x5A:				// branch always
+
+			break;
+		case 0x5B:				// branch if A<B
+
+			break;
+		case 0x5D:				// branch if less than A
+
+			break;
+		case 0x64:				// halt opcode
+
+			break;
+		default:				// invalid opcode
+			cerr << "Invalid opcode!" << endl;
+			break;
+		}
 	}
 
 }
